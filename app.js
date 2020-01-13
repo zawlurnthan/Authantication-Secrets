23ead4f34035eabc2f3f462d69fb265fa7277a98
Zaw Than <zawlurnthan@gmail.com>
2020-01-13 01:57:36 -0600

    change encryption to hash method

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
 
 // giving example how to use secret file
-//console.log(process.env.API_KEY);
+console.log(md5("123456"));
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
@@ -21,9 +21,6 @@ const userSchema = new mongoose.Schema ({
   email: String,
   password: String
 });
-
-// Encrypting password
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ["password"]});
 
 const User = mongoose.model("User", userSchema);
 
@@ -46,7 +43,7 @@ app.get("/logout", function(req, res){
 app.post("/register", function(req, res){
   const newUser = new User ({
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   });
   newUser.save(function(err){
     if(err){
@@ -59,7 +56,7 @@ app.post("/register", function(req, res){
 
 app.post("/login", function(req, res){
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
 
   User.findOne({email: username}, function(err, foundUser){
     if(err){
